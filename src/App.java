c0cc28df8d3286839e4a81543d1dbe9bb12d8205
Murilo Scheffel <murilo.scheffel@outlook.com>
2025-03-10 18:18:32 -0300

    solicitação 2

--- src/App.java
+++ src/App.java
@@ -3,18 +3,18 @@ public class App {
 
         Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
         Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
-
+        Carro utilitario = new Carro("Utilitario", TipoCombustivel.DIESEL, 5, 70);
 
         System.out.println("Tipos de veiculos:");
-        System.out.println(esportivo);
+        System.out.println(utilitario);
 
         System.out.println("\n\n----------------");
-        System.out.println("\nAbastencendo carro esportivo com gasolina");
-        esportivo.abastece(TipoCombustivel.GASOLINA, 55);
-        System.out.println(esportivo);
-        System.out.println("\nViajando com o carro esportivo");
-        esportivo.viaja(250);
-        esportivo.viaja(150);
-        System.out.println(esportivo);
+        System.out.println("\nAbastencendo carro utilitario com diesel");
+        utilitario.abastece(TipoCombustivel.DIESEL, 70);
+        System.out.println(utilitario);
+        System.out.println("\nViajando com o carro utilitario");
+        utilitario.viaja(250);
+        utilitario.viaja(150);
+        System.out.println(utilitario);
     }
 }
